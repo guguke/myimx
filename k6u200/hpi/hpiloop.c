f6e0fe6b35e3627b7755d354a425a39f2108cde8
gbm618 <gbm618@gmail.com>
2017-06-13 13:50:39 +0800

    v2 send.cmd.first 13ec.b.1 13ec.4.1 13ec.6.0

--- k6u200/hpi/hpiloop.c
+++ k6u200/hpi/hpiloop.c
@@ -10,6 +10,9 @@ int main(int argc,char *argv[])
 	int loop=1000;
 	int i,len;
 
+	printf(" usage: ./hpiloop [loop number (default:1000)]\n");
+	printf(" usage: ./hpiloop \n");
+	printf(" usage: ./hpiloop 10000\n");
 	if(argc>1)loop=atoi(argv[1]);
 
 	fd=open("/proc/myhpi",O_RDWR);
@@ -17,10 +20,33 @@ int main(int argc,char *argv[])
 		printf("open proc.myhpi error\n");
 		return -2;
 	}
+
+	p16[0]=0x13ec;
+	p16[1]=0x0b;
+	p16[2]=0x1;
+	write(fd,p16,6);
+	printf("  send.cmd : 0x%x 0x%x 0x%x\n",p16[0],p16[1],p16[2]);
+
+
+	p16[0]=0x13ec;
+	p16[1]=0x4;
+	p16[2]=0x1;
+	write(fd,p16,6);
+	printf("  send.cmd : 0x%x 0x%x 0x%x\n",p16[0],p16[1],p16[2]);
+
+	p16[0]=0x13ec;
+	p16[1]=0x6;
+	p16[2]=0x0;
+	write(fd,p16,6);
+	printf("  send.cmd : 0x%x 0x%x 0x%x\n",p16[0],p16[1],p16[2]);
+
+	printf(" start loop test\n");
 	for(i=0;i<loop;i++){
 		len=read(fd,p16,512);
 		if(len > 0) write(fd,p16,len);
+		printf("\r read.write loop : %d",i+1);
 	}
+	printf("\n");
 	close(fd);
 	return 0;
 }
