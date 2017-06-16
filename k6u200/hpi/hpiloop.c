454abfbc12b0090d32bc651017b986bd0e9b2f4e
gbm618 <gbm618@gmail.com>
2017-06-16 18:26:13 +0800

    v3 remove 13ec.b.1 13ec.4.1 13ec.6.0  , just loop send

--- k6u200/hpi/hpiloop.c
+++ k6u200/hpi/hpiloop.c
@@ -10,6 +10,7 @@ int main(int argc,char *argv[])
 	int loop=1000;
 	int i,len;
 
+	printf(" hpiloop, just loop send , should use hpicmd.0x13ec.0x6.0x1.... first\n");
 	printf(" usage: ./hpiloop [loop number (default:1000)]\n");
 	printf(" usage: ./hpiloop \n");
 	printf(" usage: ./hpiloop 10000\n");
@@ -20,7 +21,7 @@ int main(int argc,char *argv[])
 		printf("open proc.myhpi error\n");
 		return -2;
 	}
-
+#if 0
 	p16[0]=0x13ec;
 	p16[1]=0x0b;
 	p16[2]=0x1;
@@ -36,10 +37,10 @@ int main(int argc,char *argv[])
 
 	p16[0]=0x13ec;
 	p16[1]=0x6;
-	p16[2]=0x0;
+	p16[2]=0x1;
 	write(fd,p16,6);
 	printf("  send.cmd : 0x%x 0x%x 0x%x\n",p16[0],p16[1],p16[2]);
-
+#endif
 	printf(" start loop test\n");
 	for(i=0;i<loop;i++){
 		len=read(fd,p16,512);
